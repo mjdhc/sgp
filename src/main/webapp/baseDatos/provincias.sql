cf954bc3094310c5b484a9113a5c950f3d3e2258
mjdh <mjdh@minjusticia.gob.ec>
2019-02-12 17:01:17 -0500

    ok

--- src/main/webapp/baseDatos/provincias.sql
+++ src/main/webapp/baseDatos/provincias.sql
@@ -1785,7 +1785,7 @@ INSERT INTO "public"."sj_sis_civil" ("civ_id", "civ_nombre") VALUES ('4', 'VIUDO
 INSERT INTO "public"."sj_sis_civil" ("civ_id", "civ_nombre") VALUES ('5', 'UNION LIBR');
 
 
-
+prueba
 
 
 
